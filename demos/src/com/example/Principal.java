dd52728ea1a3a27659f5965738fd74caf931373b
Javier <jmartin@grupoloyal.com>
2021-02-08 14:09:33 +0100

    Ejemplos

--- demos/src/com/example/Principal.java
+++ demos/src/com/example/Principal.java
@@ -3,7 +3,20 @@ package com.example;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		int i = 4_000_000, j, k = 1;
+		final String nombre;
+		nombre = "Mundo";
+		j = 5;
+		i = j + k;
+		System.out.println("Hola " + nombre + "\nResultado: " + i);
+		var x = (float)4.0;
+		long larga = i;
+		i = (int)larga;
+		byte s = Byte.parseByte("0");
+		Object o = new Principal();
+		//((Principal)o).
 	}
-
+	public void algo() {
+		
+	}
 }
